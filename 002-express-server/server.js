3804d3aec56d30cfafa5344b59bf37f5de093fc2
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-04-02 01:47:59 +0600

    added router in different file

--- 002-express-server/server.js
+++ 002-express-server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = require("./router");
 const port = 5000;
 
 const server = express();
@@ -7,14 +8,4 @@ server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
-server.get("/hello", (req, res) => {
-  res.send("Hello World");
-});
-
-server.get("/hola", (req, res) => {
-  res.send("Hola Mundo");
-});
-
-server.get("/me", (req, res) => {
-  res.status(200).json({ name: "John Doe", age: 30, location: "New York" });
-});
+server.use("/api", router);
