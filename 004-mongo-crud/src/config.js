7ef4d29d9102acfb4c3c3d2ad288a5e6e8b8735d
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-04-08 01:21:30 +0600

    database connection successfully done

---
+++ 004-mongo-crud/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+    PORT: 5000,
+    mongodb: {
+        url: 'mongodb://localhost:27017/Products',
+    }
+};
+
+export default config;
---
+++ 004-mongo-crud/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import config from "./config.js";
+
+async function connectMongoDB() {
+    try{
+        await mongoose.connect(config.mongodb.url);
+        console.log("Connected to MongoDB");
+    }catch(error){
+        console.log("Error connecting to MongoDB", error);
+    }
+}
+
+function initializeServer() {
+  const app = express();
+
+  app.listen(config.PORT, () => {
+    console.log(`Server listening on port ${config.PORT}`);
+  });
+
+  connectMongoDB();
+}
+
+initializeServer();
